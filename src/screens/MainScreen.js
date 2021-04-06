f57e4d06ead8458193731295c38afbc088b90cb4
Ivan Shitikov <ifinesse2406@gmail.com>
2021-04-06 19:54:16 +0300

    72nd lesson finished

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,25 +1,62 @@
 import React from "react";
-import { StyleSheet, View, Text, Button, FlatList } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  FlatList,
+  Platform,
+} from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { Post } from "../components/Post";
-import {DATA} from '../data'
+import { DATA } from "../data";
+import { AppHeaderIcon } from "../components/AppHeaderIcon";
+import { Ionicons } from "@expo/vector-icons";
+import { THEME } from "../theme";
 
 export const MainScreen = ({ navigation }) => {
   const onOpenHandler = (post) => {
-    navigation.navigate("Post", {postId: post.id, date: post.date});
+    navigation.navigate("Post", { postId: post.id, date: post.date });
   };
   return (
     <View style={styles.wrapper}>
-      <FlatList data={DATA} keyExtractor={post => post.id.toString()} renderItem={( {item} ) => <Post post={item} onOpen={onOpenHandler}/>}/>
+      <FlatList
+        data={DATA}
+        keyExtractor={(post) => post.id.toString()}
+        renderItem={({ item }) => <Post post={item} onOpen={onOpenHandler} />}
+      />
     </View>
   );
 };
 
 MainScreen.navigationOptions = {
   headerTitle: "Main",
+  headerRight: () => {
+    return (
+      <View style={styles.cameraIconContainer}>
+        <Ionicons
+          color={Platform.OS === "android" ? "#fff" : THEME.MAIN_COLOR}
+          name="camera"
+          size={24}>
+        </Ionicons>
+      </View>
+    );
+  },
 };
 
 const styles = StyleSheet.create({
-  wrapper: {
-
-  },
+  wrapper: {},
+  cameraIconContainer: {
+      width: 90,
+      height: 90,
+      padding: 30,
+    //   backgroundColor: 'green'
+  }
 });
+
+//   headerRight: (<Ionicons.Button><Ionicons />),
+//   headerRight: (
+//       <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+//         <Item title='take photo' iconName='camera' onPress={() => {}}/>
+//       </HeaderButtons>
+//   )
