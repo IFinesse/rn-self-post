9b323a31565ce02b4f54fd6c5914cd81e55f85eb
Ivan Shitikov <ifinesse2406@gmail.com>
2021-04-06 17:54:02 +0300

    71st lesson finished

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,12 +1,44 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  ImageBackground,
+  ScrollView,
+  Button,
+  Alert,
+} from "react-native";
+import { DATA } from "../data";
+import { THEME } from "../theme";
 
 export const PostScreen = ({ navigation }) => {
   const postId = navigation.getParam("postId");
+
+  const post = DATA.find((p) => p.id === postId);
+
+  const onRemove = () => {
+    Alert.alert(
+        'Removing posts',
+        "Are you sure you want to remove the post?",
+        [
+          {
+            text: "Cancel",
+            style: "cancel"
+          },
+          { text: "Remove", style:'destructive', onPress: () => {}}
+        ]
+      );
+  }
+
   return (
-    <View style={styles.center}>
-      <Text>{postId}</Text>
-    </View>
+    <ScrollView>
+      <ImageBackground source={{ uri: post.img }} style={styles.image}></ImageBackground>
+      <View style={styles.TextWrap}>
+        <Text style={styles.text}>{post.text}</Text>
+      </View>
+      <Button title='Remove' onPress={onRemove} color={THEME.DANGER_COLOR}></Button>
+    </ScrollView>
   );
 };
 
@@ -22,5 +54,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  TextWrap: {
+    padding: 10
+  },
+  text: {
+    fontFamily: 'OpenSans_400Regular',
+  },
+  image: {
+    width: "100%",
+    height: 200
   },
 });
