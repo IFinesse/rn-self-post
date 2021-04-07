b2ce094731a70cf9ed4e4c79827492deaac533fa
Ivan Shitikov <ifinesse2406@gmail.com>
2021-04-07 16:17:32 +0300

    75th lesson finished

--- src/screens/FavouritesScreen.js
+++ src/screens/FavouritesScreen.js
@@ -1,18 +1,73 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  FlatList,
+  Platform,
+} from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { Post } from "../components/Post";
+import { DATA } from "../data";
+import { AppHeaderIcon } from "../components/AppHeaderIcon";
+import { Ionicons } from "@expo/vector-icons";
+import { THEME } from "../theme";
 
-export const FavouritesScreen = ({}) => {
+export const FavouritesScreen = ({ navigation }) => {
+  const onOpenHandler = (post) => {
+    navigation.navigate("Post", { postId: post.id, date: post.date, booked: post.booked });
+  };
   return (
-    <View style={styles.center}>
-      <Text>FavouritesScreen</Text>
+    <View style={styles.wrapper}>
+      <FlatList
+        data={DATA.filter(post => post.booked===true)}
+        keyExtractor={(post) => post.id.toString()}
+        renderItem={({ item }) => <Post post={item} onOpen={onOpenHandler} />}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+FavouritesScreen.navigationOptions = {
+  headerTitle: "Favourites",
+  // headerRight: () => {
+  //   return (
+  //     <View style={styles.cameraIconContainer}>
+  //       <Ionicons
+  //         color={Platform.OS === "android" ? "#fff" : THEME.MAIN_COLOR}
+  //         name="camera"
+  //         size={24}>
+  //       </Ionicons>
+  //     </View>
+  //   );
+  // },
+  headerLeft: () => {
+    return (
+      <View style={styles.menuIconContainer}>
+        <Ionicons
+          color={Platform.OS === "android" ? "#fff" : THEME.MAIN_COLOR}
+          name="ios-menu"
+          size={24}>
+        </Ionicons>
+      </View>
+    );
   },
+};
+
+const styles = StyleSheet.create({
+  wrapper: {},
+  cameraIconContainer: {
+      width: 90,
+      height: 90,
+      padding: 30,
+    //   backgroundColor: 'green'
+  }
 });
+
+//   headerRight: (<Ionicons.Button><Ionicons />),
+//   headerRight: (
+//       <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+//         <Item title='take photo' iconName='camera' onPress={() => {}}/>
+//       </HeaderButtons>
+//   )
