464ffd129d55257873612d2261be1dd5ffc07f61
Ivan Shitikov <ifinesse2406@gmail.com>
2021-04-08 17:42:55 +0300

    76th lesson finished

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs'
 import { Platform } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { MainScreen } from "../screens/MainScreen";
@@ -43,23 +44,41 @@ const BookedNavigator = createStackNavigator(
   }
 );
 
-const BottomNavigator = createBottomTabNavigator( {
+const bottomTabsConfig = {
     Post: {
         screen: PostNavigator,
         navigationOptions: {
+            tabBarLabel: 'All',
             tabBarIcon: info => <Ionicons name="ios-albums" size={24} color={info.tintColor}/>
         }
     },
     Booked: {
         screen: BookedNavigator,
         navigationOptions: {
+            tabBarLabel: 'Favourites',
             tabBarIcon:  info => <Ionicons name="ios-star" size={24} color={info.tintColor}/>
         }
     }
-},{
+}
+
+const BottomNavigator = Platform.OS ==='android' ? createMaterialBottomTabNavigator(bottomTabsConfig, {
+    activeTintColor: '#fff',
+    shifting: true,
+    barStyle: {
+        backgroundColor: THEME.MAIN_COLOR
+    }
+}) : createBottomTabNavigator (bottomTabsConfig, {
     tabBarOptions: {
         activeTintColor: THEME.MAIN_COLOR
     }
 })
+
+// createBottomTabNavigator( {
+    
+// },{
+//     tabBarOptions: {
+//         activeTintColor: THEME.MAIN_COLOR
+//     }
+// })
 
 export const AppNavigation = createAppContainer(BottomNavigator);
