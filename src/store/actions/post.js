dc3631f18474efa3a314f190be0c2d4c2c6a15e2
Ivan Shitikov <ifinesse2406@gmail.com>
2021-04-15 19:20:33 +0300

    95th lesson finished

--- src/store/actions/post.js
+++ src/store/actions/post.js
@@ -1,51 +1,55 @@
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from "expo-file-system";
 import { ADD_POST, LOAD_POSTS, REMOVE_POST, TOGGLE_BOOKED } from "../types";
-import {DB} from '../../db'
-
+import { DB } from "../../db";
 
 export const loadPosts = () => {
-    return async dispatch => {
-       const posts = await DB.getPosts()
-       dispatch ({
-            type: LOAD_POSTS,
-            payload: posts
-        })
-    } 
-} 
+  return async (dispatch) => {
+    const posts = await DB.getPosts();
+    dispatch({
+      type: LOAD_POSTS,
+      payload: posts,
+    });
+  };
+};
 
-export const toggleBooked = id => ( {
+export const toggleBooked = (post) => async (dispatch) => {
+  await DB.updatePost(post);
+  dispatch({
     type: TOGGLE_BOOKED,
-    payload: id
-} )
+    payload: post.id,
+  });
+};
 
-export const removePost = id => ({
+export const removePost = (id) => async (dispatch) => {
+    await DB.removePost(id)
+  dispatch({
     type: REMOVE_POST,
     payload: id,
-})
+  });
+};
 
-export const addPost = post => async dispatch => {
+export const addPost = (post) => async (dispatch) => {
+  console.log(post.img, "xxxxxxxx", FileSystem.documentDirectory);
 
-    console.log(post.img, "xxxxxxxx", FileSystem.documentDirectory);
+  const fileName = post.img.split("/").pop();
+  const newPath = FileSystem.documentDirectory + fileName;
 
-    const fileName = post.img.split('/').pop()
-    const newPath = FileSystem.documentDirectory + fileName
+  try {
+    await FileSystem.moveAsync({
+      to: newPath,
+      from: post.img,
+    });
+  } catch (e) {
+    console.log("error:", e);
+  }
 
-    try {
-        await FileSystem.moveAsync({
-            to: newPath,
-            from: post.img
-        })
-    } catch (e) {
-        console.log("error:", e);
-    }
+  const payload = { ...post, img: newPath };
 
-    const payload = {...post, img: newPath}
-   
-    const id = await DB.createPost(payload)
-    payload.id = id;
+  const id = await DB.createPost(payload);
+  payload.id = id;
 
-    dispatch({
-        type: ADD_POST,
-        payload
-    })
-}
+  dispatch({
+    type: ADD_POST,
+    payload,
+  });
+};
